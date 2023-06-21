412f9c55ae2f0bbb987743aa76f897ee673b6590
Diego Maggessi <diegosmteixeira@gmail.com>
2023-06-20 23:38:37 -0300

    project created

---
+++ Ventania/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { TopMenuComponent } from './components/top-menu/top-menu.component';
+
+@NgModule({
+  declarations: [
+    TopMenuComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    TopMenuComponent
+  ]
+})
+export class SharedModule { }
