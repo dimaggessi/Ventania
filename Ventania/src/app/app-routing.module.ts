4ef424c14446f25e15990dd1a5b49067c192f2f0
Diego Maggessi <diegosmteixeira@gmail.com>
2023-06-25 17:25:47 -0300

    feat: add pages/gallery module

--- Ventania/src/app/app-routing.module.ts
+++ Ventania/src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-  { path: 'book', loadChildren: () => import('./pages/book/book.module').then(m => m.BookModule) }
+  { path: 'book', loadChildren: () => import('./pages/book/book.module').then(m => m.BookModule) },
+  { path: 'gallery', loadChildren: () => import('./pages/gallery/gallery.module').then(m => m.GalleryModule) }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
